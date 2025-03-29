26f9284f13a2126ae07308a9fa0702c4a533c149
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-29 18:07:46 +0530

    Added webview , add prooer icons , fix bugs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons'; 
-
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: { display: 'none' }
+      }}
+    >
       <Tabs.Screen 
         name="index"
         options={{
           title: "Home",
           headerShown: false,
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />
         }}
       />
       {/* Add more tabs as needed */}
